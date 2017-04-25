bd43fb116861a4551045b245aefd567327761a6e
Hawk84 <paweljastrzabek@o2.pl>
2017-04-25 15:36:19 +0200

    #1 Solution for task - TwoToOne

---
+++ PioneerCSharp/PioneerCSharp/7_kyu/TwoToOne.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PioneerCSharp._7_kyu
+{
+    public class TwoToOne
+    {
+
+        public static string sortLetter(string firstWord, string secondWord)
+        {
+            string word = firstWord + secondWord;
+            string finalResult = "";
+            char[] splitWord = word.ToCharArray().Select(s => s).OrderBy(s => s).Distinct().ToArray();
+
+            foreach (char elem in splitWord)
+            {
+                finalResult = finalResult + elem.ToString();
+            }
+            
+            return finalResult;
+        }
+    }
+}
---
+++ PioneerCSharp/PionnerCSharp.Test/7_kyu_test/TwoToOneTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PioneerCSharp._7_kyu;
+
+namespace PionnerCSharp.Test
+{
+    [TestClass]
+    public class TwoToOneTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("abc", TwoToOne.sortLetter("abc","abc"));
+        }
+
+        [TestMethod]
+        public void IsDistinct()
+        {
+            Assert.AreEqual("abcdx", TwoToOne.sortLetter("bbac","xxcdd"));
+        }
+    }
+}
