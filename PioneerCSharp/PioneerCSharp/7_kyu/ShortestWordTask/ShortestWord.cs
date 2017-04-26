99c33ed50e088df18d01db5e57d3aa0870c7549f
Hawk84 <paweljastrzabek@o2.pl>
2017-04-26 14:22:35 +0200

    #2 Solution for task ShortestWord

---
+++ PioneerCSharp/PioneerCSharp/7_kyu/ShortestWordTask/ShortestWord.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PioneerCSharp._7_kyu.ShortestWord
+{
+    public class ShortestWord
+    {
+        public static int Compute(string statements)
+        {
+            String[] arr = statements.Split(' ');
+            int longestWord = int.MaxValue;
+
+            foreach(string word in arr)
+            {
+                longestWord = Math.Min(longestWord, word.Length);
+            }
+
+            return longestWord;
+        }
+
+        public static int ComputeTuning(string statements)
+        {
+            return statements.Split(' ').Min(s => s.Length);
+        }
+    }
+}
---
+++ PioneerCSharp/PionnerCSharp.Test/7_kyu_test/ShortestWordTask/ShortestWordTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PioneerCSharp._7_kyu.ShortestWord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PionnerCSharp.Test._7_kyu_test.ShortestWordTask
+{
+    [TestClass]
+    public class ShortestWordTest
+    {
+        [TestMethod]
+        public void DoesShowTheShortestWord()
+        {
+            Assert.AreEqual(5, ShortestWord.Compute("Stomil Legia"));
+        }
+    }
+}
