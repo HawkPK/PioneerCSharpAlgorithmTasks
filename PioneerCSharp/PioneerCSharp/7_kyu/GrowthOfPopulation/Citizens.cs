d10f3fe5e76e54b662d0639441488617ca7d265a
Hawk84 <paweljastrzabek@o2.pl>
2017-04-26 15:08:45 +0200

    #3 Solution for GrowthOfPopulation

---
+++ PioneerCSharp/PioneerCSharp/7_kyu/GrowthOfPopulation/Citizens.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PioneerCSharp._7_kyu.GrowthOfPopulation
+{
+    public class Citizens
+    {
+        private static int year;
+
+        public static int GrowPopulation(int population, double growth, int newCitizens, int goal)
+        {
+            year = 0;
+            while (goal > population)
+            {
+                population = population + (int)(population * (growth / 100)) + newCitizens;
+                year += 1;
+            }
+            return year;
+        }
+    }
+}
---
+++ PioneerCSharp/PionnerCSharp.Test/7_kyu_test/GrowthOfPopulationTest/CitizensTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PioneerCSharp._7_kyu.GrowthOfPopulation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PionnerCSharp.Test._7_kyu_test.GrowthOfPopulationTest
+{
+    [TestClass]
+    public class CitizensTestt
+    {
+        [TestMethod]
+        public void DoesCityReach1200After2Years()
+        {
+            Assert.AreEqual(15, Citizens.GrowPopulation(1500, 5, 100, 5000));
+            Assert.AreEqual(10, Citizens.GrowPopulation(1500000, 2.5, 10000, 2000000));
+            Assert.AreEqual(94, Citizens.GrowPopulation(1500000, 0.25, 1000, 2000000));
+        }
+    }
+}
