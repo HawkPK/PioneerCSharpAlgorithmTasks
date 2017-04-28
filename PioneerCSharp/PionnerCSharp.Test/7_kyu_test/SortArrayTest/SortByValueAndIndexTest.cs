183d8deafb30ab4bde483e5bcdd610facdfac437
Hawk84 <paweljastrzabek@o2.pl>
2017-04-28 14:29:26 +0200

    #4 Init solution for SortByValueAndIndex project

---
+++ PioneerCSharp/PionnerCSharp.Test/7_kyu_test/SortArrayTest/SortByValueAndIndexTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PionnerCSharp.Test._7_kyu_test.SortArrayTest
+{
+    public class SortByValueAndIndexTest
+    {
+        //Comment
+    }
+}
