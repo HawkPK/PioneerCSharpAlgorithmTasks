de76e90a823a4a7f5b4a2a98b72db00776b741ea
Hawk84 <paweljastrzabek@o2.pl>
2017-05-08 18:58:30 +0200

    #6 Solution for ExesAndOhs Task (included refactor and no refactor method)

---
+++ PioneerCSharp/PioneerCSharp/7_kyu/ExesAndOhs/ExesAndOhs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PioneerCSharp._7_kyu.Exes_and_Ohs
+{
+    public class ExesAndOhs
+    {
+        public static bool Compute(string word)
+        {
+            char[] arr = word.ToCharArray();
+            int counterX = 0;
+            int counterO = 0;
+            foreach (char item in arr)
+            {
+                if(Char.ToUpper(item) == 'O')
+                {
+                    counterO++;
+                } else if (Char.ToUpper(item) == 'X')
+                {
+                    counterX++;
+                }
+            }
+
+            return counterO == counterX ? true : false;
+        }
+
+        public static bool ComputeRef(string word)
+        {
+            return word.ToUpper().Count(c => c == 'X') == word.ToUpper().Count(c => c == 'O');
+        }
+    }
+}
---
+++ PioneerCSharp/PionnerCSharp.Test/7_kyu_test/ExesAndOhsTest/ExesAndOhsTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PioneerCSharp._7_kyu.Exes_and_Ohs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PionnerCSharp.Test._7_kyu_test.ExesAndOhsTest
+{
+    [TestClass]
+    public class ExesAndOhsTest
+    {
+        [TestMethod]
+        public void IsNumberOfXANDOEqual()
+       { 
+            Assert.AreEqual(true, ExesAndOhs.Compute("ooxx"));
+            Assert.AreEqual(true, ExesAndOhs.ComputeRef("ooxx"));
+        }
+
+        [TestMethod]
+        public void IsNumberOfXANDONotEqual()
+        {
+            Assert.AreEqual(false, ExesAndOhs.Compute("ooxxx"));
+            Assert.AreEqual(false, ExesAndOhs.ComputeRef("ooxxx"));
+        }
+
+        [TestMethod]
+        public void IsIgnoreCaseSensitive()
+        {
+            Assert.AreEqual(true, ExesAndOhs.Compute("ooXxA"));
+            Assert.AreEqual(true, ExesAndOhs.ComputeRef("ooXxA"));
+        }
+    }
+}
