978645aa091e0b075b58c7adfbf3a9d23908160f
Hawk84 <paweljastrzabek@o2.pl>
2017-05-10 22:27:37 +0200

    #8 Solution for HiddenWord Task (does not included refactor)

---
+++ PioneerCSharp/PioneerCSharp/7_kyu/TheHiddenWord/HiddenWord.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PioneerCSharp._7_kyu.TheHiddenWord
+{
+    public class HiddenWord
+    {
+        public static string Hide(int number)
+        {
+            Char[] array = number.ToString().ToCharArray();
+            string result = "";
+            foreach (char item in array)
+            {
+                result = (result + ReturnLetter(item));
+            }
+            return result;
+        }
+
+        private static string ReturnLetter(char letter)
+        {
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+            dict.Add(6,"a");
+            dict.Add(1,"b");
+            dict.Add(7,"d");
+            dict.Add(4,"e");
+            dict.Add(3,"i");
+            dict.Add(2,"l");
+            dict.Add(9,"m");
+            dict.Add(8,"n");
+            dict.Add(0,"o");
+            dict.Add(5,"t");
+
+            return dict[(int) Char.GetNumericValue(letter)];
+        }
+    }
+}
