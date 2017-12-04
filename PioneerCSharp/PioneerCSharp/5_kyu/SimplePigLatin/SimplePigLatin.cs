a949425d88824f2b048e87182cd1a866657ab1f5
Hawk84 <paweljastrzabek@o2.pl>
2017-12-04 18:52:55 +0100

    #13 Solution for SimplePigLatin(without refactor)

---
+++ PioneerCSharp/PioneerCSharp/5_kyu/SimplePigLatin/SimplePigLatin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PioneerCSharp._5_kyu.SimplePigLatin
+{
+    public static class SimplePigLatin
+    {
+        public const string lastLetters = "ay";
+        public static string PigIt(string str)
+        {
+            string[] arr = str.Split(' ');
+            var newWord = "";
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var otherLetters = arr[i].Substring(1);
+                var firstLetter = arr[i].Substring(0, 1);
+                if (Regex.Match(firstLetter, @"[a-zA-Z]").Success)
+                {
+                    newWord += otherLetters + firstLetter + lastLetters;
+                } else
+                {
+                    newWord += firstLetter;
+                }
+
+                if (i != arr.Length - 1)
+                {
+                    newWord += " ";
+                }
+            }
+            return newWord;
+        }
+    }
+}
---
+++ PioneerCSharp/PionnerCSharp.Test/5_kuy_test/SimplePigLatinTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using PioneerCSharp._5_kyu.SimplePigLatin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PionnerCSharp.Test._5_kuy_test
+{
+    [TestFixture]
+    public class SimplePigLatinTest
+    {
+        [Test]
+        public void PigItTest()
+        {
+            Assert.AreEqual("elloHay orldWay !", SimplePigLatin.PigIt("Hello World !"));     // elloHay orldWay !
+            Assert.AreEqual("igPay atinlay siay oolcay", SimplePigLatin.PigIt("Pig latin is cool"));
+            Assert.AreEqual("hisTay siay ymay tringsay", SimplePigLatin.PigIt("This is my string"));
+        }
+    }
+}
